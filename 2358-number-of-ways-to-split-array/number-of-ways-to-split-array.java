5c0c48a2e3222ca43dfd98df2a453d7021f980d6
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-01-03 08:13:05 +0530

    Time: 4 ms (16.81%) | Memory: 59.2 MB (41.17%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -4,22 +4,21 @@ class Solution {
         long[] suffixSum=new long[nums.length];
         
         long sum=0;
-        for(int a:nums){
-            sum+=a;
-        }
+      
         for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
             if(i==0){
                 prefixSum[i]=nums[i];
-                suffixSum[i]=sum-prefixSum[i];
+                
             }
             else{
                 prefixSum[i]=prefixSum[i-1]+nums[i];
-                suffixSum[i]=sum-prefixSum[i];
+                
             }
         }
         int count=0;
         for(int i=0;i<nums.length-1;i++){
-            if(prefixSum[i]>=suffixSum[i]){
+            if(prefixSum[i]>=sum-prefixSum[i]){
                 count++;
             }
         }
