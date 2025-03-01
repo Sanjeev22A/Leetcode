93828bc7efbc88ac08f3819dfbf6195d8715d08b
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-03-01 06:28:44 +0530

    Time: 22 ms (64.74%) | Memory: 50.5 MB (49.84%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,68 +1,50 @@
 class Solution {
-
     public String shortestCommonSupersequence(String str1, String str2) {
-        int str1Length = str1.length();
-        int str2Length = str2.length();
-
-        int[][] dp = new int[str1Length + 1][str2Length + 1];
-
-        // Initialize the base cases
-        // When str2 is empty, the supersequence is str1 itself (length = row index)
-        for (int row = 0; row <= str1Length; row++) {
-            dp[row][0] = row;
+        int[][] dp=new int[str1.length()+1][str2.length()+1];
+        int row=str1.length()+1;
+        int col=str2.length()+1;
+        for(int i=0;i<row;i++){
+            //at 0th column length of str2=0 and hence output=length(str1)
+            dp[i][0]=i;
         }
-        // When str1 is empty, the supersequence is str2 itself (length = col index)
-        for (int col = 0; col <= str2Length; col++) {
-            dp[0][col] = col;
+        for(int j=0;j<col;j++){
+            dp[0][j]=j;
         }
-
-        // Fill the DP table
-        for (int row = 1; row <= str1Length; row++) {
-            for (int col = 1; col <= str2Length; col++) {
-                if (str1.charAt(row - 1) == str2.charAt(col - 1)) {
-                    // If characters match, inherit the length from the diagonal +1
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
-                } else {
-                    // If characters do not match, take the minimum length option +1
-                    dp[row][col] =
-                        Math.min(dp[row - 1][col], dp[row][col - 1]) + 1;
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    //Not longest subsequence but here we calculate the shortest supersequence
+                    dp[i][j]=Math.min(dp[i][j-1],dp[i-1][j])+1;
                 }
             }
         }
-
-        StringBuilder supersequence = new StringBuilder();
-        int row = str1Length, col = str2Length;
-
-        while (row > 0 && col > 0) {
-            if (str1.charAt(row - 1) == str2.charAt(col - 1)) {
-                // If characters match, take it from diagonal
-                supersequence.append(str1.charAt(row - 1));
-                row--;
-                col--;
-            } else if (dp[row - 1][col] < dp[row][col - 1]) {
-                // If str1’s character is part of the supersequence, move up
-                supersequence.append(str1.charAt(row - 1));
-                row--;
-            } else {
-                // If str2’s character is part of the supersequence, move left
-                supersequence.append(str2.charAt(col - 1));
-                col--;
+        StringBuilder result=new StringBuilder();
+        int i=row-1,j=col-1;
+        while(i>0 && j>0){
+            if(str1.charAt(i-1)==str2.charAt(j-1)){
+                result.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else if(dp[i-1][j]<dp[i][j-1]){
+                result.append(str1.charAt(i-1));
+                i--;
+            }else{
+                result.append(str2.charAt(j-1));
+                j--;
             }
         }
-
-        // Append any remaining characters
-        // If there are leftover characters in str1
-        while (row > 0) {
-            supersequence.append(str1.charAt(row - 1));
-            row--;
+        while(i>0){
+            result.append(str1.charAt(i-1));
+            i--;
         }
-        // If there are leftover characters in str2
-        while (col > 0) {
-            supersequence.append(str2.charAt(col - 1));
-            col--;
+        while(j>0){
+            result.append(str2.charAt(j-1));
+            j--;
         }
+        return result.reverse().toString();
 
-        // Reverse the built sequence to get the correct order
-        return supersequence.reverse().toString();
     }
 }
