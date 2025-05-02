58fe0ff27f76e9ef7758db839951cba85f8d70c3
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-02 08:29:53 +0530

    Time: 29 ms (24.91%) | Memory: 46.1 MB (29.43%) - LeetSync

--- 868-push-dominoes/push-dominoes.java
+++ 868-push-dominoes/push-dominoes.java
@@ -1,50 +1,43 @@
 class Solution {
     public String pushDominoes(String dominoes) {
         int n=dominoes.length();
-        //We take right as positive
-        //We take left as negative
         int[] left=new int[n];
         int[] right=new int[n];
-        //Right account
         int force=0;
         for(int i=0;i<n;i++){
             if(dominoes.charAt(i)=='R'){
                 force=n;
-                right[i]=force;
             }
             else if(dominoes.charAt(i)=='L'){
                 force=0;
-            }
-            else{
+
+            }else{
                 force=Math.max(force-1,0);
-                right[i]=force;
             }
+            right[i]=force;
         }
         force=0;
         for(int i=n-1;i>=0;i--){
             if(dominoes.charAt(i)=='L'){
                 force=-n;
-                left[i]=force;
-            }
-            else if(dominoes.charAt(i)=='R'){
+            }else if(dominoes.charAt(i)=='R'){
                 force=0;
+            }else{
+                force=Math.min(force+1,0);
             }
-            else{
-                force=Math.min(0,force+1);
-                left[i]=force;
-            }
+            left[i]=force;
         }
         StringBuilder res=new StringBuilder();
         for(int i=0;i<n;i++){
             int cur=left[i]+right[i];
-            if(cur<0){
-                res.append('L');
-            }
-            else if(cur>0){
+            if(cur>0){
                 res.append('R');
+            }else if(cur<0){
+                res.append('L');
             }else{
                 res.append('.');
             }
+            
         }
         return res.toString();
     }
