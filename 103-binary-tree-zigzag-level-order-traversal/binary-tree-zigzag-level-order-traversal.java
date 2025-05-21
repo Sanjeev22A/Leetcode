f41bfedf4ee706501ce64a985b4a48f545099150
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-21 07:51:35 +0530

    Time: 1 ms (68.35%) | Memory: 42.3 MB (58.49%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,43 +15,40 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        int level=1;
+        Deque<TreeNode> dq=new ArrayDeque<>();
+        List<List<Integer>> res=new ArrayList<>();
         if(root==null){
             return new ArrayList<>();
         }
-        List<List<Integer>> res=new ArrayList<>();
-        Deque<TreeNode> dq=new ArrayDeque<>();
-        dq.addLast(root);
+        dq.add(root);
+        int level=1;
         while(!dq.isEmpty()){
             int a=dq.size();
-            List<Integer> t=new ArrayList<>();
             
+            List<Integer> temp=new ArrayList<>();
             for(int i=0;i<a;i++){
-                TreeNode temp=null;
                 if(level==1){
-                    temp=dq.pollFirst();
-                    t.add(temp.val);
-                    if(temp.left!=null){dq.addLast(temp.left);}
-                    if(temp.right!=null){dq.addLast(temp.right);}
-                }else{
-                    temp=dq.pollLast();
-                    t.add(temp.val);
-                    if(temp.right!=null){
-                        dq.addFirst(temp.right);
+                    TreeNode q=dq.pollFirst();
+                    temp.add(q.val);
+                    if(q.left!=null){
+                        dq.addLast(q.left);
                     }
-                    if(temp.left!=null){
-                        dq.addFirst(temp.left);
+                    if(q.right!=null){
+                        dq.addLast(q.right);
                     }
                 }
-                
-                
+                else{
+                    TreeNode q=dq.pollLast();
+                    temp.add(q.val);
+                    if(q.right!=null){
+                        dq.addFirst(q.right);
+                    }if(q.left!=null){
+                        dq.addFirst(q.left);
+                    }
+                }
             }
-            
-
-
             level=(level+1)%2;
-            System.out.println(level);
-            res.add(t);
+            res.add(temp);
         }
         return res;
     }
