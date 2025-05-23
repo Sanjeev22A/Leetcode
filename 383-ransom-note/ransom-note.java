689d805e51ce656ad067a52b6ded10a035b356eb
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-23 19:59:17 +0530

    Time: 1 ms (99.40%) | Memory: 44.8 MB (69.24%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] ransom=new int[26];
+        int[] mag=new int[26];
+        for(char c:ransomNote.toCharArray()){
+            ransom[c-'a']++;
+        }
+        for(char c:magazine.toCharArray()){
+            mag[c-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(ransom[i]>mag[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
