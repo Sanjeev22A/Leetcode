44ca3bed01ed8943d58629e0f5e238ef32b35d9c
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-24 20:22:29 +0530

    Time: 8 ms (42.33%) | Memory: 44.7 MB (28.61%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        //We use dp where dp[j] means substring from 0 to j-1 belongs to dict
+        boolean[] dp=new boolean[s.length()+1]; //We need one more than length as we need substring until length
+        dp[0]=true; //Empty string  in dict
+
         HashSet<String> dict=new HashSet<>();
         for(String a:wordDict){
             dict.add(a);
         }
-
-        boolean[] dp=new boolean[s.length()+1];
-        dp[0]=true;
         for(int i=0;i<s.length();i++){
             for(int j=0;j<=i;j++){
-                if(dp[j]==true && dict.contains(s.substring(j,i+1))){
+                if(dp[j]==true //First condition substring from 0 to j-1 belongs
+                && dict.contains(s.substring(j,i+1))){  //Substring from j to i belongs
                     dp[i+1]=true;
-                }
+                }  
             }
         }
         return dp[s.length()];
