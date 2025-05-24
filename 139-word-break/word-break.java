e90e65ca8a2e58b7a5849c94c9f7c8d68fb619d6
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-24 20:09:43 +0530

    Time: 8 ms (42.33%) | Memory: 44.7 MB (37.68%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -4,15 +4,14 @@ class Solution {
         for(String a:wordDict){
             dict.add(a);
         }
+
         boolean[] dp=new boolean[s.length()+1];
         dp[0]=true;
         for(int i=0;i<s.length();i++){
             for(int j=0;j<=i;j++){
-                
                 if(dp[j]==true && dict.contains(s.substring(j,i+1))){
                     dp[i+1]=true;
                 }
-
             }
         }
         return dp[s.length()];
