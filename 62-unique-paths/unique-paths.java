b29b280a83c1d1e3bc47e585429dbc0c03b35d2b
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-27 08:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (17.50%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,45 +1,18 @@
 class Solution {
-    class Pair{
-        int i,j;
-        
-        Pair(int i,int j){
-            this.i=i;
-            this.j=j;
-        }
-         public int hashCode() {
-            return 31 * i + j; // A common way to generate hash codes for multiple fields
-        }
-        public boolean equals(Object o){
-            if(o instanceof Pair){
-                Pair cur=(Pair)o;
-                return cur.i==this.i&&cur.j==this.j;
-            }
-            return false;
-        }
-    }
-    HashMap<Pair,Integer> memo=new HashMap<>();
-    int logic(int i,int j,int m,int n){
-        Pair p=new Pair(i,j);
-        if(memo.containsKey(p)){
-            return memo.get(p);
-        }
-        if(i>=m || j>=n){
-            return 0;
-        }
-        if(i==m-1 && j==n-1){
-            
-            memo.put(p,1);
-            return 1;
-        }
-        int val1=logic(i,j+1,m,n);
-        int val2=logic(i+1,j,m,n);
-        int result=val1+val2;
-        memo.put(p,result);
-
-        return result;
-
-    }
     public int uniquePaths(int m, int n) {
-        return logic(0,0,m,n);
-    }   
+        int[][] grid=new int[m][n];
+        grid[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    continue;
+                }else{
+                    int top=i>0?grid[i-1][j]:0;
+                    int left=j>0?grid[i][j-1]:0;
+                    grid[i][j]=top+left;
+                }
+            }
+        }
+        return grid[m-1][n-1];
+    }
 }
