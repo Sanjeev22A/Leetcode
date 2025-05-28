03a3d0c899571f39d254c3e4b945990f18e85272
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-05-28 21:11:36 +0530

    Time: 4 ms (24.40%) | Memory: 42.8 MB (24.34%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,14 +1,13 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-          boolean[] inStack=new boolean[26];
         Stack<Character> st=new Stack<>();
+        boolean[] inStack=new boolean[26];
         Map<Character,Integer> lastIndex=new HashMap<>();
         int i=0;
         for(char c:s.toCharArray()){
             lastIndex.put(c,i);
             i++;
         }
-
         i=0;
         for(char c:s.toCharArray()){
             if(st.isEmpty()){
@@ -18,27 +17,24 @@ class Solution {
             else if(inStack[c-'a']){
                 i++;
                 continue;
-            }else if(!inStack[c-'a'] && st.peek()>c ){
+            }else if(st.peek()>c){
                 while(!st.isEmpty() && st.peek()>c && lastIndex.get(st.peek())>i){
                     char d=st.pop();
-                    System.out.println(d);
                     inStack[d-'a']=false;
                 }
                 st.push(c);
-               
                 inStack[c-'a']=true;
-            }else{
+            }
+            else{
                 st.push(c);
                 inStack[c-'a']=true;
             }
-
-
             i++;
         }
-        StringBuilder sb=new StringBuilder();
+        StringBuilder res=new StringBuilder();
         for(char c:st){
-            sb.append(c);
+            res.append(c);
         }
-        return sb.toString();
+        return res.toString();
     }
 }
