40f98077627259b15961851d35ae5f2ffb3c1754
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-04 08:46:04 +0530

    Time: 7 ms (80.85%) | Memory: 55.2 MB (50.29%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.java
+++ 881-loud-and-rich/loud-and-rich.java
@@ -1,31 +1,35 @@
 class Solution {
     int[] dp;
-
     public int[] loudAndRich(int[][] richer, int[] quiet) {
-        int n = quiet.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-        for (int[] r : richer) graph.get(r[1]).add(r[0]);
-
-        for (int i = 0; i < n; i++) {
-            dfs(i, graph, quiet);
+        int n=quiet.length;
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
         }
-        return dp;
+        for(int[] a:richer){
+            graph.get(a[1]).add(a[0]);  //we traverse from the poor to the rich person
+        }
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=dfs(i,graph,quiet);
+        }
+        return ans;
+
     }
-
-    int dfs(int node, List<List<Integer>> graph, int[] quiet) {
-        if (dp[node] != -1) return dp[node];
-
-        int minPerson = node;
-        for (int richerPerson : graph.get(node)) {
-            int candidate = dfs(richerPerson, graph, quiet);
-            if (quiet[candidate] < quiet[minPerson]) {
-                minPerson = candidate;
+    int dfs(int person,List<List<Integer>> graph,int[] quiet){
+        if(dp[person]!=-1){
+            return dp[person];
+        }
+        int quietPerson=person;
+        for(int a:graph.get(person)){
+            int minPerson=dfs(a,graph,quiet);
+            if(quiet[minPerson]<quiet[quietPerson]){
+                quietPerson=minPerson;
             }
         }
-        return dp[node] = minPerson;
+        dp[person]=quietPerson;
+        return dp[person];
     }
 }
