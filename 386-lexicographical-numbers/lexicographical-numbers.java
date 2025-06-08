6927c8944c9f78c45c71817955467e46fb2a5c4b
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-08 09:23:00 +0530

    Time: 4 ms (71.50%) | Memory: 48.7 MB (34.13%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -4,10 +4,10 @@ class Solution {
         res=new ArrayList<>();
         for(int i=1;i<=9;i++){
             dfs(i,n);
-
         }
         return res;
     }
+
 
     void dfs(int current,int n){
         if(current>n){
@@ -17,6 +17,6 @@ class Solution {
         for(int i=0;i<=9;i++){
             dfs(current*10+i,n);
         }
+        
     }
-
 }
