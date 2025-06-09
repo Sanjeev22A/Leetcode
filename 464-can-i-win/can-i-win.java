d281ffdc0d505627e0f5b2a88ca3d3ec46bce378
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-09 21:32:59 +0530

    Time: 466 ms (27.62%) | Memory: 90.8 MB (9.14%) - LeetSync

--- 464-can-i-win/can-i-win.java
+++ 464-can-i-win/can-i-win.java
@@ -1,34 +1,32 @@
 class Solution {
-    Map<Integer,Boolean> map;
+    Map<Integer,Boolean> memo;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        map=new HashMap<>();
+        memo=new HashMap<>();
         if(desiredTotal==0){
             return true;
         }
         int a=maxChoosableInteger*(maxChoosableInteger+1)/2;
-        System.out.println(a);
         if(a<desiredTotal){
             return false;
         }
         return dfs(0,0,desiredTotal,maxChoosableInteger);
     }
 
-    boolean dfs(int mask,int currentTotal,int desiredTotal,int maxChoosableInteger){
-        if(map.containsKey(mask)){
-            return map.get(mask);
+    boolean dfs(int curTotal,int mask,int desiredTotal,int maxChoosableInteger){
+        if(memo.containsKey(mask)){
+            return memo.get(mask);
         }
-
-        if(currentTotal>=desiredTotal){
+        if(curTotal>=desiredTotal){
             return false;
         }
         boolean flag=false;
         for(int i=1;i<=maxChoosableInteger;i++){
-            if((mask&(1<<i))==0 && !dfs(mask|(1<<i),currentTotal+i,desiredTotal,maxChoosableInteger)){
+            if((mask&(1<<i))==0 && !dfs(curTotal+i,(mask|(1<<i)),desiredTotal,maxChoosableInteger)){
                 flag=true;
                 break;
             }
         }
-        map.put(mask,flag);
+        memo.put(mask,flag);
         return flag;
     }
 }
