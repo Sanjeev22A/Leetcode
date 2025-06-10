601e4ba61984bdf20dc1e5f7aa95781d585ae852
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-10 21:18:09 +0530

    Time: 99 ms (5.05%) | Memory: 71.2 MB (5.19%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,70 +1,59 @@
-class Pair {
-    int a, count;
-    Pair(int a, int count) {
-        this.a = a;
-        this.count = count;
+class Pair{
+    int val,count;
+    Pair(int val,int count){
+        this.val=val;
+        this.count=count;
     }
 }
 
-public class UnionFind {
-    HashMap<Integer, Pair> parent;
-
-    UnionFind(int[] arr) {
-        parent = new HashMap<>();
-        for (int a : arr) {
-            parent.put(a, new Pair(a, 1));
+class UnionFind{
+    Map<Integer,Pair> parent;
+    UnionFind(int[] nums){
+        parent=new HashMap<>();
+        for(int a:nums){
+            parent.put(a,new Pair(a,1));
         }
     }
-
-    int find(int x) {
-        if (x != parent.get(x).a) {
-            parent.get(x).a = find(parent.get(x).a);  
+    int find(int a){
+        if(a==parent.get(a).val){
+            return a;
         }
-        return parent.get(x).a;
+        parent.get(a).val=find(parent.get(a).val);
+        return parent.get(a).val;
     }
 
-    int union(int a, int b) {
-        if (!parent.containsKey(a) || !parent.containsKey(b)) return 0;
-
-        int rootA = find(a);
-        int rootB = find(b);
-
-        if (rootA == rootB) return parent.get(rootA).count;
-
-        
-        int newCount = parent.get(rootA).count + parent.get(rootB).count;
-        parent.get(rootB).a = rootA;
-        parent.get(rootA).count = newCount;
-
-        return newCount;
+    int union(int a,int b){
+        Pair rootA=parent.get(find(a));
+        Pair rootB=parent.get(find(b));
+        if(rootA.val==rootB.val){
+            return rootA.count;
+        }
+        parent.get(rootB.val).val=rootA.val;
+        parent.get(rootA.val).count=rootA.count+rootB.count;
+        return rootA.count+rootB.count;
     }
 
-    int getMaxComponent() {
-        int max = 0;
-        for (Map.Entry<Integer, Pair> entry : parent.entrySet()) {
-            
-            if (entry.getKey() == parent.get(entry.getKey()).a) {
-                max = Math.max(entry.getValue().count, max);
-            }
+    int getMax(){
+        int max=0;
+        for(Map.Entry<Integer,Pair> a:parent.entrySet()){
+            max=Math.max(a.getValue().count,max);
         }
         return max;
     }
 }
-
 class Solution {
     public int longestConsecutive(int[] nums) {
         UnionFind uf=new UnionFind(nums);
-        HashSet<Integer> a=new HashSet<>();
-        for(int b:nums){
-            a.add(b);
+        HashSet<Integer> set=new HashSet<>();
+        for(int a:nums){
+            set.add(a);
         }
-
-        for(int b:a){
-            if(a.contains(b+1)){
-                uf.union(b,b+1);
+        for(int a:set){
+            if(set.contains(a+1)){
+                uf.union(a,a+1);
+               
             }
         }
-        return uf.getMaxComponent();
-        
+        return uf.getMax();
     }
 }
