45ebda9fd494bced5359eacfab7cd7e2b88971bf
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-19 19:57:14 +0530

    Time: 828 ms (5.01%) | Memory: 46 MB (8.24%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,13 +1,15 @@
 class Solution {
-  
-    private int getIndex(char c) {
+    
+    int getIndex(char c) {
         if (c >= 'a' && c <= 'z') return c - 'a';
-        return 26 + (c - 'A');
+        else return 26 + (c - 'A');
     }
 
     boolean checkEqual(int[] map1, int[] map2) {
         for (int i = 0; i < 52; i++) {
-            if (map1[i] < map2[i]) return false;
+            if (map1[i] < map2[i]) {
+                return false;
+            }
         }
         return true;
     }
@@ -19,23 +21,43 @@ class Solution {
             map2[getIndex(c)]++;
         }
 
-        int start = 0, end = 0, minLen = Integer.MAX_VALUE;
-        String result = "";
-
-        while (end < s.length()) {
-            map1[getIndex(s.charAt(end))]++;
+        int start = 0, end = 0;
+        int ans = Integer.MAX_VALUE;
+        String temp = "";
+        boolean startUpdate=false;
+        while (start<=end && end < s.length()) {
+            char c = s.charAt(end);
+            if(!startUpdate){
+                map1[getIndex(c)]++;
+            }
             
-            while (checkEqual(map1, map2)) {
-                if ((end - start + 1) < minLen) {
-                    minLen = end - start + 1;
-                    result = s.substring(start, end + 1);
+
+            if (checkEqual(map1, map2)) {
+                if (end - start + 1 < ans) {
+                    ans = end - start + 1;
+                    temp = s.substring(start, end + 1);
+                    
+
                 }
                 map1[getIndex(s.charAt(start))]--;
                 start++;
+                startUpdate=true;
+            } else {
+                end++;
+                startUpdate=false;
             }
-            end++;
+            System.out.println(start+":"+end);
         }
 
-        return result;
+        while (start<=end && checkEqual(map1, map2)) {
+            if (end - start + 1 < ans) {
+                temp = s.substring(start, end + 1);
+            }
+            System.out.println(s.substring(start,end+1));
+            map1[getIndex(s.charAt(start))]--;
+            start++;
+        }
+
+        return temp;
     }
 }
