73757c4f26ef48323900c478fd95797e635ca01f
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-23 08:18:12 +0530

    Time: 1 ms (98.76%) | Memory: 43.7 MB (35.38%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,34 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ class Pair{
+    TreeNode key;
+    int index;
+    Pair(TreeNode key,int index){
+        this.key=key;
+        this.index=index;
+    }
+ }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
+        if(root==null){
+            return 0;
+        }
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(root,0));
+        int maxWidth=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            Pair top=q.peek();
+            int minIndex=top.index;
+            for(int i=0;i<size;i++){
+                Pair cur=q.poll();
+                int curIndex=cur.index;
+                maxWidth=Math.max(maxWidth,curIndex-minIndex+1);
+                TreeNode t=cur.key;
+                if(t.left!=null){
+                    q.offer(new Pair(t.left,2*curIndex));
 
-        int maxWidth = 0;
-        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(root, 0));
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int minIndex = q.peek().getValue();  
-            int first = 0, last = 0;
-
-            for (int i = 0; i < size; i++) {
-                Pair<TreeNode, Integer> current = q.poll();
-                TreeNode node = current.getKey();
-                int index = current.getValue() - minIndex; 
-
-                if (i == 0) first = index;
-                if (i == size - 1) last = index;
-
-                if (node.left != null) {
-                    q.offer(new Pair<>(node.left, 2 * index));
                 }
-                if (node.right != null) {
-                    q.offer(new Pair<>(node.right, 2 * index + 1));
+                if(t.right!=null){
+                    q.offer(new Pair(t.right,2*curIndex+1));
                 }
             }
-            maxWidth = Math.max(maxWidth, last - first + 1);
         }
-
         return maxWidth;
     }
 }
