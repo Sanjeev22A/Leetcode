a4804ced945bc970279e436d1ba1a8111a51aea4
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-26 07:47:45 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (67.02%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -1,8 +1,7 @@
 class Solution {
     public int longestSubsequence(String s, int k) {
         int length=(int)(Math.log(k)/Math.log(2))+1;
-        int sm=0;
-        int cnt=0;
+        int cnt=0,sm=0;
         int bitPos=0;
         for(int i=s.length()-1;i>=0;i--){
             char c=s.charAt(i);
@@ -10,8 +9,8 @@ class Solution {
                 cnt++;
             }else{
                 if(bitPos<length && sm+(1<<bitPos)<=k){
-                    cnt++;
                     sm+=(1<<bitPos);
+                    cnt++;
                 }
             }
             bitPos++;
