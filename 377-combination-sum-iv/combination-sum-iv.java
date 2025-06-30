06b67e47058671ba8f13d154d630900de2a0b774
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-06-30 17:29:22 +0530

    Time: 6 ms (3.93%) | Memory: 41.2 MB (46.07%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,23 +1,23 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp=new int[target+1];
-        TreeSet<Integer> set=new TreeSet<>();
+        Set<Integer> set=new HashSet<>();
         for(int a:nums){
             set.add(a);
         }
+        List<Integer> lst=new ArrayList<>(set);
+        Collections.sort(lst);
+        
         dp[0]=1;
-        if(set.contains(1)){
-            dp[1]=dp[0];
-        }
-        System.out.println(dp[1]);
-        for(int i=2;i<=target;i++){
-            for(int a:set){
-                if(a<=i){
-                    dp[i]+=dp[i-a];
+        for(int i=1;i<=target;i++){
+            for(int a:lst){
+                if(a>i){
+                    break;
                 }
+                dp[i]+=dp[i-a];
             }
+            //System.out.println(i+":"+dp[i]);
         }
         return dp[target];
-
     }
 }
