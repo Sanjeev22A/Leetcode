24c3bcc68766c8186a8b537ab6194a4c29a2597c
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-03 12:53:33 +0530

    Time: 159 ms (5.42%) | Memory: 47.4 MB (6.40%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.java
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.java
@@ -1,49 +1,51 @@
 class Solution {
     int[][] score;
-    int m;
-    Map<Integer, Integer> memo;
-
-    int dp(int studentUsed, int mentorUsed) {
-        int key = (studentUsed << m) | mentorUsed;
-        if (memo.containsKey(key)) return memo.get(key);
-
-        if (studentUsed == (1 << m) - 1) {
-            memo.put(key, 0);
+    int maxVal;
+    Map<Integer,Integer> memo;
+    int dp(int studentUsed,int mentorUsed,int m){
+        int key=(studentUsed<<m)+mentorUsed;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        if(studentUsed==maxVal || mentorUsed==maxVal){
+            memo.put(key,0);
             return 0;
         }
-
-        int maxScore = 0;
-        for (int i = 0; i < m; i++) {
-            if ((studentUsed & (1 << i)) != 0) continue;
-            for (int j = 0; j < m; j++) {
-                if ((mentorUsed & (1 << j)) == 0) {
-                    int temp1 = studentUsed | (1 << i);
-                    int temp2 = mentorUsed | (1 << j);
-                    int curScore = score[i][j] + dp(temp1, temp2);
-                    maxScore = Math.max(maxScore, curScore);
+        int maxScore=0;
+        for(int i=0;i<m;i++){
+            if((studentUsed & 1<<i) !=0){
+                continue;
+            }
+            for(int j=0;j<m;j++){
+                if((mentorUsed & 1<<j)==0){
+                    int temp1=studentUsed | 1<<i;
+                    int temp2=mentorUsed | 1<<j;
+                    int curScore=this.score[i][j]+dp(temp1,temp2,m);
+                    maxScore=Math.max(curScore,maxScore);
                 }
             }
         }
-
-        memo.put(key, maxScore);
+        memo.put(key,maxScore);
         return maxScore;
     }
-
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
-        m = students.length;
-        score = new int[m][m];
-        memo = new HashMap<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                int val = 0;
-                for (int k = 0; k < students[0].length; k++) {
-                    if (students[i][k] == mentors[j][k]) val++;
+        memo=new HashMap<>();
+        int m=students.length;
+        this.score=new int[m][m];
+        maxVal=(1<<m)-1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                int val=0;
+                for(int k=0;k<students[i].length;k++){
+                    if(students[i][k]==mentors[j][k]){
+                        val++;
+                    }
                 }
-                score[i][j] = val;
+                score[i][j]=val; 
             }
         }
 
-        return dp(0, 0);
+        return dp(0,0,m);
+       
     }
 }
