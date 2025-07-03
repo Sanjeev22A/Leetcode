2a388faf1285f096914df60a9ec3cd5ff5cb839f
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-03 11:42:10 +0530

    Time: 114 ms (39.38%) | Memory: 46.2 MB (6.08%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,25 @@
 class Solution {
+    Map<String,Integer> memo;
     int ways(int[] nums,int currentIndex,int curVal,int target){
+        String cur=curVal+":"+currentIndex;
+        if(memo.containsKey(cur)){
+            return memo.get(cur);
+        }
         if(currentIndex==nums.length){
             if(curVal==target){
-
+                memo.put(cur,1);
                 return 1;
             }
+            memo.put(cur,0);
             return 0;
         }
-        return ways(nums,currentIndex+1,curVal+nums[currentIndex],target)
+        int val=ways(nums,currentIndex+1,curVal+nums[currentIndex],target)
             + ways(nums,currentIndex+1,curVal-nums[currentIndex],target);
+        memo.put(cur,val);
+        return val;
     }
     public int findTargetSumWays(int[] nums, int target) {
+        memo=new HashMap<>();
         return ways(nums,0,0,target);
     }
 }
