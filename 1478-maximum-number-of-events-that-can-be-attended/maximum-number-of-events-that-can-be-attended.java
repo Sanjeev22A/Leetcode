c4883b4cf75795efcf063ed7914d093f06b4698f
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-07 07:30:58 +0530

    Time: 34 ms (98.25%) | Memory: 73.9 MB (99.53%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,38 +1,41 @@
-class UnionFind{
-    int[] parent;
-    UnionFind(int n){
-        parent=new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-    }
-    int search(int day){
-        if(day==parent[day]){   //That means this day is unoccupied
-            return day;
-        }
-        parent[day]=search(parent[day]); //Occupied
-        return parent[day];
-    }
-}
-
 class Solution {
+    class UnionFind{
+        int[] parent;
+        UnionFind(int n){
+            parent=new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            }
+        }
+        int search(int day){
+            if(day==parent[day]){  //Implies a free day
+                return day;
+            }
+            parent[day]=search(parent[day]);
+            return parent[day];
+        }
+        void markDay(int day){
+            parent[day]=search(day+1);  //Next available day will be present as parent of this day
+        }
+    }
     public int maxEvents(int[][] events) {
         Arrays.sort(events,(a,b)->{
             return a[1]-b[1];
-        });  //Sort by end dates in ascending order
-        int maxDay=events[events.length-1][1]+2;
-        UnionFind uf=new UnionFind(maxDay);
+        });
 
-        int ans=0;
+        int maxDay=events[events.length-1][1]+2;
+        UnionFind uf=new UnionFind(maxDay+2);
+        int count=0;
         for(int[] a:events){
             int start=a[0];
             int end=a[1];
-            int availableEnd=uf.search(a[0]);
-            if(availableEnd<=end){
-                ans++;
-                uf.parent[availableEnd]=uf.search(availableEnd+1);
+            int availableDay=uf.search(start);
+            if(availableDay<=end){
+                count++;
+                uf.markDay(availableDay);
             }
+           
         }
-        return ans;
+        return count;
     }
 }
