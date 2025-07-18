4ebad5678ed3507b4745ea83ef81c6da21f1311b
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-18 08:47:25 +0530

    Time: 328 ms (95.68%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,24 +1,18 @@
-SELECT id, visit_date, people
-FROM (
-    SELECT *,
-           ROW_NUMBER() OVER (ORDER BY id) - 
-           ROW_NUMBER() OVER (PARTITION BY people >= 100 ORDER BY id) AS grp
-    FROM stadium
-) AS t
-WHERE people >= 100
-  AND grp IN (
-    SELECT grp
-    FROM (
-        SELECT grp, COUNT(*) AS cnt
-        FROM (
-            SELECT *,
-                   ROW_NUMBER() OVER (ORDER BY id) - 
-                   ROW_NUMBER() OVER (PARTITION BY people >= 100 ORDER BY id) AS grp
-            FROM stadium
-        ) AS temp
-        WHERE people >= 100
-        GROUP BY grp
-        HAVING COUNT(*) >= 3
-    ) AS valid_groups
+# Write your MySQL query statement below
+with temp_cte as(
+    select id,visit_date,LAG(id) over win as id_prev,LAG(id,2) over win as id_prev_2,people,LAG(people) over win as people_prev,LAG(people,2) over win as people_prev_2 from stadium window win as (order by id)
+),answer_cte as(
+    select id,visit_date,people from temp_cte where people>=100 and people_prev>=100 and people_prev_2>=100 and id_prev=id-1 and id_prev_2=id-2 
+),
+temp_cte_2 as(
+    select id,visit_date,LEAD(id) over win as id_prev,LEAD(id,2) over win as id_prev_2,people,LEAD(people) over win as people_prev,LEAD(people,2) over win as people_prev_2 from stadium window win as (order by id)
+),answer_cte_2 as(
+    select id,visit_date,people from temp_cte_2 where people>=100 and people_prev>=100 and people_prev_2>=100 and id_prev=id+1 and id_prev_2=id+2 
+),
+temp_cte_3 as(
+    select id,visit_date,LEAD(id) over win as id_prev,LAG(id) over win as id_prev_2,people,LEAD(people) over win as people_prev,LAG(people) over win as people_prev_2 from stadium window win as (order by id)
+),answer_cte_3 as(
+    select id,visit_date,people from temp_cte_3 where people>=100 and people_prev>=100 and people_prev_2>=100 and id_prev=id+1 and id_prev_2=id-1 
 )
-ORDER BY id;
+#select * from answer_cte_3;
+select * from answer_cte_2 union select * from answer_cte union select * from answer_cte_3 order by id;
