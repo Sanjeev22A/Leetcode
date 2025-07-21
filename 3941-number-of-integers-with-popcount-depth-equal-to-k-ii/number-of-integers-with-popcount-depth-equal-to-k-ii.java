f6f2c65150292ae984631db8629c63be70d74177
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-21 13:03:43 +0530

    Time: 88 ms (100.00%) | Memory: 95.7 MB (99.42%) - LeetSync

--- 3941-number-of-integers-with-popcount-depth-equal-to-k-ii/number-of-integers-with-popcount-depth-equal-to-k-ii.java
+++ 3941-number-of-integers-with-popcount-depth-equal-to-k-ii/number-of-integers-with-popcount-depth-equal-to-k-ii.java
@@ -1,120 +1,90 @@
 class Solution {
-    private static final int MAX_DEPTH = 6;
-    private final Map<Long, Integer> depthCache = new HashMap<>();
-    
-    // Precompute popcount depth for numbers 0-64 (covers all possible bit counts)
-    private void precomputeDepths() {
-        depthCache.put(0L, Integer.MAX_VALUE); // Special case for 0
-        depthCache.put(1L, 0);
-        
-        for (int i = 2; i <= 64; i++) {
-            depthCache.put((long)i, 1 + depthCache.get((long)Long.bitCount(i)));
+    Map<Long,Integer> dp;
+    int popDepth(long num){
+
+        if(num==0){
+            return 0;
         }
-    }
-    
-    private int findPopDepth(long val) {
-        if (val == 0L) return Integer.MAX_VALUE; // Or handle as needed
-        if (val == 1L) return 0;
-        
-        if (depthCache.containsKey(val)) {
-            return depthCache.get(val);
+        if(dp.containsKey(num)){
+            return dp.get(num);
         }
-        
-        int setBits = Long.bitCount(val);
-        int depth = 1 + findPopDepth(setBits);
-        depthCache.put(val, depth);
-        return depth;
+        if(num==1){
+            dp.put(1L,0);
+            return 0;
+        }
+        dp.put(num,1+popDepth(Long.bitCount(num)));
+        return dp.get(num);
     }
-    
     public int[] popcountDepth(long[] nums, long[][] queries) {
-        precomputeDepths();
-        
-        int n = nums.length;
-        int[] depths = new int[n];
-        
-        // Initialize depths array
-        for (int i = 0; i < n; i++) {
-            depths[i] = findPopDepth(nums[i]);
+        dp=new HashMap<>();
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=popDepth(nums[i]);
+            
         }
-        
-        // Use Fenwick Trees for efficient range sum queries and updates
-        FenwickTree[] trees = new FenwickTree[MAX_DEPTH + 1];
-        for (int i = 0; i <= MAX_DEPTH; i++) {
-            trees[i] = new FenwickTree(n);
-        }
-        
-        // Initialize Fenwick Trees
-        for (int i = 0; i < n; i++) {
-            int depth = depths[i];
-            if (depth <= MAX_DEPTH) {
-                trees[depth].update(i, 1);
+        FenwickTree ft=new FenwickTree(arr);
+        List<Integer> ans=new ArrayList<>();
+        for(long[] a:queries){
+            if(a[0]==1){
+                ans.add(ft.find((int)a[1],(int)a[2],(int)a[3]));
+            }
+            else{
+                int v1=ft.nums[(int)a[1]];
+                int v2=popDepth(a[2]);
+                ft.updateWrapper((int)a[1],v1,v2);
             }
         }
-        
-        List<Integer> results = new ArrayList<>();
-        
-        for (long[] query : queries) {
-            if (query[0] == 1) { // Range query
-                int l = (int) query[1];
-                int r = (int) query[2];
-                int k = (int) query[3];
-                
-                if (k <= MAX_DEPTH) {
-                    int count = trees[k].query(r) - (l > 0 ? trees[k].query(l - 1) : 0);
-                    results.add(count);
-                } else {
-                    results.add(0);
-                }
-            } else { // Update query
-                int idx = (int) query[1];
-                long newVal = query[2];
-                
-                int oldDepth = depths[idx];
-                int newDepth = findPopDepth(newVal);
-                
-                // Update the arrays
-                nums[idx] = newVal;
-                depths[idx] = newDepth;
-                
-                // Update Fenwick Trees
-                if (oldDepth <= MAX_DEPTH) {
-                    trees[oldDepth].update(idx, -1);
-                }
-                if (newDepth <= MAX_DEPTH) {
-                    trees[newDepth].update(idx, 1);
-                }
-            }
+        int[] res=new int[ans.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=ans.get(i);
         }
+        return res;
         
-        return results.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
-// Fenwick Tree (Binary Indexed Tree) for efficient range sum queries
-class FenwickTree {
-    private final int[] tree;
-    private final int n;
-    
-    public FenwickTree(int size) {
-        this.n = size;
-        this.tree = new int[n + 1];
+class FenwickTree{
+    int[] nums;
+    int[][] prefixArr;
+    FenwickTree(int[] nums){
+        this.nums=nums;
+        prefixArr=new int[8][nums.length+1];
+        build();
     }
-    
-    public void update(int idx, int delta) {
-        idx++; // Convert to 1-indexed
-        while (idx <= n) {
-            tree[idx] += delta;
-            idx += idx & (-idx);
+    void build(){
+        for(int i=0;i<nums.length;i++){
+            update(i+1,nums[i],1);
         }
     }
-    
-    public int query(int idx) {
-        idx++; // Convert to 1-indexed
-        int sum = 0;
-        while (idx > 0) {
-            sum += tree[idx];
-            idx -= idx & (-idx);
+    int twoComp(int index){
+        return -index;
+    }
+    int parent(int index){
+        return index-(index&twoComp(index));
+    }
+    int next(int index){
+        return index+(index&twoComp(index));
+    }
+    void updateWrapper(int index,int oldK,int newK){
+        update(index+1,oldK,-1);
+        update(index+1,newK,1);
+        nums[index]=newK;
+    }
+    void update(int index,int k,int val){
+        if(index>=prefixArr[0].length){
+            return;
         }
-        return sum;
+        prefixArr[k][index]+=val;
+        update(next(index),k,val);
+    }
+    
+    int getEnd(int index,int k){
+        if(index<=0){
+            return 0;
+        }
+        return prefixArr[k][index]+getEnd(parent(index),k);
+    }
+    int find(int l,int r,int k){
+        return getEnd(r+1,k)-getEnd(l,k);
     }
 }
