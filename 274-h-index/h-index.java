51b26955b0a2b054deef6925f4eba2be901dd15e
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-23 21:33:36 +0530

    Time: 2 ms (70.78%) | Memory: 42 MB (12.44%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,17 +1,16 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int[] freqArray=new int[1001];
-        for(int citation:citations){
-            freqArray[citation]++;
+       int[] freqArray=new int[1001];
+       for(int a:citations){
+            freqArray[a]++;
+       }
+       int prevSeen=0;
+       for(int i=1000;i>=0;i--){
+        prevSeen+=freqArray[i];
+        if(prevSeen>=i){
+            return i;
         }
-        int hIndex;
-        for(int i=999;i>=0;i--){
-            freqArray[i]=freqArray[i+1]+freqArray[i];
-            if(freqArray[i]>=i){
-                hIndex=i;
-                return hIndex;
-            }
-        }
-        return -1;
+       }
+       return 0;
     }
 }
