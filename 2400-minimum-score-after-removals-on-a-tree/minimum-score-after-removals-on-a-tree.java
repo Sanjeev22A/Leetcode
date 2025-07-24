683e6502db7a23451dcdc75dfe981662e1014c05
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-24 18:58:18 +0530

    Time: 31 ms (75.86%) | Memory: 44.9 MB (72.41%) - LeetSync

--- 2400-minimum-score-after-removals-on-a-tree/minimum-score-after-removals-on-a-tree.java
+++ 2400-minimum-score-after-removals-on-a-tree/minimum-score-after-removals-on-a-tree.java
@@ -1,71 +1,85 @@
 class Solution {
-
-    int res = Integer.MAX_VALUE;
+    int time = 0;
 
     public int minimumScore(int[] nums, int[][] edges) {
         int n = nums.length;
-        List<List<Integer>> e = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            e.add(new ArrayList<>());
-        }
-        for (int[] v : edges) {
-            e.get(v[0]).add(v[1]);
-            e.get(v[1]).add(v[0]);
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; ++i) graph.add(new ArrayList<>());
+        for (int[] e : edges) {
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
 
-        int sum = 0;
-        for (int x : nums) {
-            sum ^= x;
-        }
+        int[] xor = new int[n];
+        int[] in = new int[n];
+        int[] out = new int[n];
+        dfs(0, -1, graph, nums, xor, in, out);
 
-        dfs(0, -1, nums, e, sum);
-        return res;
+        int totalXor = xor[0];
+        int minScore = Integer.MAX_VALUE;
+
+        for (int i = 0; i < edges.length; ++i) {
+            int a = edges[i][0], b = edges[i][1];
+            if (in[a] < in[b]) {
+                // a is parent of b
+            } else {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            for (int j = i + 1; j < edges.length; ++j) {
+                int c = edges[j][0], d = edges[j][1];
+                if (in[c] < in[d]) {
+                    // c is parent of d
+                } else {
+                    int temp = c;
+                    c = d;
+                    d = temp;
+                }
+
+                if (isDescendant(d, b, in, out)) {
+                    // d in b-subtree
+                    int x1 = xor[d];
+                    int x2 = xor[b] ^ xor[d];
+                    int x3 = totalXor ^ xor[b];
+                    minScore = Math.min(minScore, getDiff(x1, x2, x3));
+                } else if (isDescendant(b, d, in, out)) {
+                    // b in d-subtree
+                    int x1 = xor[b];
+                    int x2 = xor[d] ^ xor[b];
+                    int x3 = totalXor ^ xor[d];
+                    minScore = Math.min(minScore, getDiff(x1, x2, x3));
+                } else {
+                    // Separate subtrees
+                    int x1 = xor[b];
+                    int x2 = xor[d];
+                    int x3 = totalXor ^ xor[b] ^ xor[d];
+                    minScore = Math.min(minScore, getDiff(x1, x2, x3));
+                }
+            }
+        }
+        return minScore;
     }
 
-    private int calc(int part1, int part2, int part3) {
-        return (
-            Math.max(part1, Math.max(part2, part3)) -
-            Math.min(part1, Math.min(part2, part3))
-        );
+    void dfs(int node, int parent, List<List<Integer>> graph, int[] nums, int[] xor, int[] in, int[] out) {
+        in[node] = time++;
+        xor[node] = nums[node];
+        for (int nei : graph.get(node)) {
+            if (nei != parent) {
+                dfs(nei, node, graph, nums, xor, in, out);
+                xor[node] ^= xor[nei];
+            }
+        }
+        out[node] = time++;
     }
 
-    private int dfs(int x, int f, int[] nums, List<List<Integer>> e, int sum) {
-        int son = nums[x];
-        for (int y : e.get(x)) {
-            if (y == f) {
-                continue;
-            }
-            son ^= dfs(y, x, nums, e, sum);
-        }
-
-        for (int y : e.get(x)) {
-            if (y == f) {
-                dfs2(y, x, son, x, nums, e, sum);
-            }
-        }
-        return son;
+    boolean isDescendant(int u, int v, int[] in, int[] out) {
+        return in[v] <= in[u] && out[u] <= out[v];
     }
 
-    private int dfs2(
-        int x,
-        int f,
-        int oth,
-        int anc,
-        int[] nums,
-        List<List<Integer>> e,
-        int sum
-    ) {
-        int son = nums[x];
-        for (int y : e.get(x)) {
-            if (y == f) {
-                continue;
-            }
-            son ^= dfs2(y, x, oth, anc, nums, e, sum);
-        }
-        if (f == anc) {
-            return son;
-        }
-        res = Math.min(res, calc(oth, son, sum ^ oth ^ son));
-        return son;
+    int getDiff(int a, int b, int c) {
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        return max - min;
     }
 }
