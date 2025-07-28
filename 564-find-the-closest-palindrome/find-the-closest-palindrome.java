f94e6fe42d990829cb8cd3d332f2a2f9707a0aa9
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-28 14:37:15 +0530

    Time: 14 ms (5.15%) | Memory: 42.7 MB (9.61%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.java
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.java
@@ -1,44 +1,91 @@
 class Solution {
-    public String nearestPalindromic(String n) {
-        long[] candidates=new long[5];
-        int length=n.length();
-        long number=Long.parseLong(n);
-        if(number<=10){
-            return String.valueOf(number-1);
-        }
-        if(number==11){
-            return "9";
-        }
-        long leftHalf=Long.parseLong(n.substring(0,(length+1)/2));
-        
-        boolean isEvenLength=(length%2==0);
-        candidates[0]=generatePalindrome(leftHalf,isEvenLength);
-        candidates[1]=generatePalindrome(leftHalf-1,isEvenLength);
-        candidates[2]=generatePalindrome(leftHalf+1,isEvenLength);
-        candidates[3]=(long)Math.pow(10,length-1)-1;
-        candidates[4]=(long)Math.pow(10,length)+1;
-        long difference=Long.MAX_VALUE;
-        long prev=Long.MAX_VALUE;
-        for(long candidate:candidates){
-           
-            if ((candidate != number) && (((Math.abs(number - candidate) ==difference) && candidate < prev) || (Math.abs(number - candidate) < difference)))
-{
-                difference=Math.abs(number-candidate);
-                prev=candidate;
+    long generatePalindrome(long num,boolean isEven){
+        long ans=num;
+        if(isEven){
+     
+            while(num>0){
+                ans=ans*10+num%10;
+                num=num/10;
+            }
+        }else{
+
+            num=num/10;
+            while(num>0){
+                ans=ans*10+num%10;
+                num=num/10;
             }
         }
-        return String.valueOf(prev);
+        return ans;
+    
     }
+    public String nearestPalindromic(String n) {
+        
+        long num=Long.parseLong(n);
+        if(num<=10){
+            return String.valueOf(num-1);
+        }
+        if(num==11){
+            return "9";
+        }
+        int length=n.length();
+        List<Long> candidates=new ArrayList<>();
+       
+        
+        long a1=(long)num/(long)(Math.pow(10,length/2));
+        
+        long a2=a1-1;
+        long a3=a1+1;
+        long num2=num-3;
+        int length2=String.valueOf(num2).length();
+        
+        long num3=num+3;
+        int length3=String.valueOf(num3).length();
+        //t=length2%2==1?(length2+1)/2:length2/2;
+        long a4=(long)num2/(long)(Math.pow(10,length2/2));
 
-    long generatePalindrome(long leftHalf,boolean isEvenLength){
-        long palindrome=leftHalf;
-        if(!isEvenLength){
-            leftHalf/=10;
+        long a5=a4-1;
+        long a6=a4+1;
+        //t=length3%2==1?(length3+1)/2:length3/2;
+        long a7=(long)num3/(long)(Math.pow(10,length3/2));
+        long a8=a7-1;
+        long a9=a7+1;
+        
+        candidates.add(a1);
+        candidates.add(a2);
+        candidates.add(a3);
+        candidates.add(a4);
+        candidates.add(a5);
+        candidates.add(a6);
+        candidates.add(a7);
+        candidates.add(a8);
+        candidates.add(a9);
+        long dif=Long.MAX_VALUE;
+        long ans=0;
+        int i=0;
+        for(long a:candidates){
+            int l=0;
+            if(i<3){
+                l=length;
+            }else if(i<6){
+                l=length2;
+            }else{
+                l=length3;
+            }
+
+            long numTemp=generatePalindrome(a,l%2==0);
+            System.out.println(numTemp+":"+a);
+            long tempDif=Math.abs(numTemp-num);
+            
+            if(tempDif>0){
+                if(tempDif==dif && numTemp<ans){
+                    ans=numTemp;
+                }else if(tempDif<dif){
+                    ans=numTemp;
+                    dif=tempDif;
+                }
+            }
+            i++;
         }
-        while(leftHalf>0){
-            palindrome=palindrome*10+leftHalf%10;
-            leftHalf/=10;
-        }
-        return palindrome;
+        return String.valueOf(ans);
     }
 }
