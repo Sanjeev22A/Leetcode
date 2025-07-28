452f2cbb662a758f6273224e6d41694c1f6a4097
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-28 05:46:58 +0530

    Time: 1546 ms (5.04%) | Memory: 128.4 MB (5.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,37 +1,31 @@
 class Solution {
     String s;
     Map<Long,Integer> memo;
-    int dp(int startIndex,int endIndex){
-        if(startIndex>endIndex){
+    int dp(int start,int end){
+        if(start>end){
             return 0;
         }
-        else if(startIndex==endIndex){
-            return 1;
-        }
-        Long key=((long)startIndex<<32)|endIndex;
+        long key=((long)start<<32)|end;
         if(memo.containsKey(key)){
             return memo.get(key);
         }
-        if(s.charAt(startIndex)==s.charAt(endIndex)){
-            int val=2+dp(startIndex+1,endIndex-1);
-            memo.put(key,val);
-            return val;
-
-        }else{
-            int val= Math.max(dp(startIndex+1,endIndex),dp(startIndex,endIndex-1));
+        if(start==end){
+            memo.put(key,1);
+            return 1;
+        }
+        if(s.charAt(start)==s.charAt(end)){
+            int val=2+dp(start+1,end-1);
             memo.put(key,val);
             return val;
         }
+        int v1=dp(start+1,end);
+        int v2=dp(start,end-1);
+        memo.put(key,Math.max(v1,v2));
+        return Math.max(v1,v2);
     }
     public int longestPalindromeSubseq(String s) {
         memo=new HashMap<>();
-        int n=s.length();   
         this.s=s;
-        
-       
-       return dp(0,n-1);
-       
-        
+        return dp(0,s.length()-1);
     }
-
 }
