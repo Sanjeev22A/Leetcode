480077468b661f1209b1991b907b7a83f2ebfea9
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-29 20:20:24 +0530

    Time: 701 ms (95.46%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 #select person_name from (select person_name,weight,sum(weight) over (order by turn) as cum_sum,turn from queue order by turn) as temp1 where cum_sum<=1000 order by cum_sum desc limit 1;
 with temp_cte as(
-    select person_id,person_name,sum(weight) over (order by turn) as weight from queue order by turn
+    select person_id,person_name,sum(weight) over (order by turn rows between unbounded preceding and current row) as weight from queue order by turn
 ),
 temp_cte_2 as(
     select person_name,rank() over(order by weight desc) as my_rank from temp_cte where weight<=1000
