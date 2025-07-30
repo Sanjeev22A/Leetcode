1354220c6cf9aa0fe06deeb5a017c68a2e54e658
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-30 20:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (98.60%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -1,27 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     int val;
-
-    boolean isLeaf(TreeNode cur) {
-        return cur.left == null && cur.right == null;
+    
+    boolean isLeaf(TreeNode cur){
+        if(cur.left==null && cur.right==null){
+            return true;
+        }
+        return false;
     }
-
-    void dfs(TreeNode cur, int curVal) {
-        if (cur == null) return;
-
-        curVal = (curVal << 1) | cur.val; 
-
-        if (isLeaf(cur)) {
-            val += curVal;
+    void ans(TreeNode cur,int curVal){
+        if(cur==null){
             return;
         }
+        
+        curVal=(curVal<<1)+cur.val;
+        
+        if(isLeaf(cur)){
+            
+            val+=curVal;
+            return;
+        }
+        ans(cur.left,curVal);
+        ans(cur.right,curVal);
 
-        dfs(cur.left, curVal);
-        dfs(cur.right, curVal);
     }
-
     public int sumRootToLeaf(TreeNode root) {
-        val = 0;
-        dfs(root, 0);
+        val=0;
+        ans(root,0);
         return val;
     }
 }
