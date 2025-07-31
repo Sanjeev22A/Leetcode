ea92330b07d4121ef0111cdffe8efc1bbc3c3066
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-07-31 11:32:17 +0530

    Time: 10 ms (39.63%) | Memory: 42.9 MB (5.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,35 +1,44 @@
 class Solution {
     int[] nums;
-    
+    Map<String,int[]> map;
     int[] dp(int startIndex,int endIndex,int playerTurn){
         if(startIndex>endIndex){
             return new int[]{0,0};
         }
+        String key=startIndex+":"+endIndex+":"+playerTurn;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
         int[] val1=dp(startIndex+1,endIndex,(playerTurn+1)%2);
         int[] val2=dp(startIndex,endIndex-1,(playerTurn+1)%2);
+        int[] ans=new int[2];
         if(val1[playerTurn]+nums[startIndex]>val2[playerTurn]+nums[endIndex]){
-            int[] ans=new int[2];
+            
             ans[playerTurn]=val1[playerTurn]+nums[startIndex];
             ans[1-playerTurn]=val1[1-playerTurn];
-            return ans;
+            
         }else if(val1[playerTurn]+nums[startIndex]<val2[playerTurn]+nums[endIndex]){
-            int[] ans=new int[2];
+           
             ans[playerTurn]=val2[playerTurn]+nums[endIndex];
             ans[1-playerTurn]=val2[1-playerTurn];
-            return ans;
+            
         }else if(val1[1-playerTurn]<val2[1-playerTurn]){
-            int[] ans=new int[2];
+            
             ans[playerTurn]=val1[playerTurn]+nums[startIndex];
             ans[1-playerTurn]=val1[1-playerTurn];
-            return ans;
+          
         }else{
-            int[] ans=new int[2];
+            
             ans[playerTurn]=val2[playerTurn]+nums[endIndex];
             ans[1-playerTurn]=val2[1-playerTurn];
-            return ans;
+            
         }
+        map.put(key,ans);
+        return ans;
     }
     public boolean predictTheWinner(int[] nums) {
+        map=new HashMap<>();
+        
         this.nums=nums;
         int[] ans=dp(0,nums.length-1,0);
         return ans[0]>=ans[1];
