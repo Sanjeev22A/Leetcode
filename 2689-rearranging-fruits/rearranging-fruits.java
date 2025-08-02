3eef1c96936523cf8efd5581fcdd7aeba0b227a7
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-02 07:41:50 +0530

    Time: 45 ms (69.01%) | Memory: 67.8 MB (19.72%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,28 +1,31 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-        Map<Integer, Integer> freq = new TreeMap<>();
-        int n = basket1.length;
-        for (int i = 0; i < n; i++) {
-            freq.put(basket1[i], freq.getOrDefault(basket1[i], 0) + 1);
-            freq.put(basket2[i], freq.getOrDefault(basket2[i], 0) - 1);
+        Map<Integer,Integer> freqMap=new HashMap<>();
+        int minVal=Integer.MAX_VALUE;
+        for(int a:basket1){
+            freqMap.put(a,freqMap.getOrDefault(a,0)+1);
+            minVal=Math.min(a,minVal);
         }
-
-        List<Integer> excess = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-            int value = e.getValue();
-            if (value % 2 != 0) return -1;
-            for (int i = 0; i < Math.abs(value) / 2; i++) {
+        for(int a:basket2){
+            freqMap.put(a,freqMap.getOrDefault(a,0)-1);
+            minVal=Math.min(a,minVal);
+        }
+        List<Integer> excess=new ArrayList<>();
+        for(Map.Entry<Integer,Integer> e:freqMap.entrySet()){
+            int val=Math.abs(e.getValue());
+            if(val%2==1){
+                return -1L;
+            }
+            for(int i=0;i<val/2;i++){
                 excess.add(e.getKey());
             }
         }
-
         Collections.sort(excess);
-        long minVal = Math.min(Arrays.stream(basket1).min().getAsInt(), Arrays.stream(basket2).min().getAsInt());
-        long ans = 0;
-        for (int i = 0; i < excess.size() / 2; i++) {
-            ans += Math.min(excess.get(i), 2 * minVal);
+        long ans=0L;
+        for(int i=0;i<excess.size()/2;i++){
+            
+            ans+=(long)Math.min(2*minVal,excess.get(i));
         }
-
         return ans;
     }
 }
