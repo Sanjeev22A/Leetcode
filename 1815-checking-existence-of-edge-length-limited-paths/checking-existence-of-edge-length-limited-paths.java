e74def9d7399820dbd872dbe5c21463a7ebf41ab
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-07 18:47:58 +0530

    Time: 71 ms (70.93%) | Memory: 70.9 MB (92.44%) - LeetSync

--- 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.java
+++ 1815-checking-existence-of-edge-length-limited-paths/checking-existence-of-edge-length-limited-paths.java
@@ -5,7 +5,6 @@ class UnionFind{
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-
     }
     int find(int x){
         if(x==parent[x]){
@@ -14,53 +13,45 @@ class UnionFind{
         parent[x]=find(parent[x]);
         return parent[x];
     }
+    
+    void union(int x,int y){
+        int parentx=find(x);
+        int parenty=find(y);
+        if(parentx!=parenty){
+            parent[parenty]=parentx;
+        }
+        return;
+    }
     boolean isConnected(int x,int y){
         int parentx=find(x);
         int parenty=find(y);
         return parentx==parenty;
     }
-    void union(int x,int y){
-        int parentx=find(x);
-        int parenty=find(y);
-        if(parentx==parenty){
-            return;
-        }
-        else{
-            parent[parenty]=parentx;
 
-        }
-    }
-    
 }
 class Solution {
     public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
-        boolean[] ans=new boolean[queries.length];
         int[][] queriesModified=new int[queries.length][4];
-        int idx=0;
-        for(int[] a:queries){
-            queriesModified[idx][0]=a[0];
-            queriesModified[idx][1]=a[1];
-            queriesModified[idx][2]=a[2];
-            queriesModified[idx][3]=idx;
-            idx++;
+        for(int i=0;i<queries.length;i++){
+            for(int j=0;j<3;j++){
+                queriesModified[i][j]=queries[i][j];
+            }
+            queriesModified[i][3]=i;
         }
-        UnionFind uf=new UnionFind(n);
         Arrays.sort(queriesModified,(a,b)->(a[2]-b[2]));
         Arrays.sort(edgeList,(a,b)->(a[2]-b[2]));
-        int i=0;
-        for(int[] a:queriesModified){
-            while(i<edgeList.length && edgeList[i][2]<a[2]){
-                uf.union(edgeList[i][0],edgeList[i][1]);
-                i++;
+        boolean[] ans=new boolean[queries.length];
+        int j=0;
+        UnionFind uf=new UnionFind(n);
+        for(int i=0;i<queriesModified.length;i++){
+            while(j<edgeList.length && edgeList[j][2]<queriesModified[i][2]){
+                uf.union(edgeList[j][0],edgeList[j][1]);
+                j++;
             }
-            if(uf.isConnected(a[0],a[1])){
-                ans[a[3]]=true;
-            }else{
-                ans[a[3]]=false;
+            if(uf.isConnected(queriesModified[i][0],queriesModified[i][1])){
+                ans[queriesModified[i][3]]=true;
             }
-
         }
         return ans;
     }
-    
 }
