4f72f4817ead8c05a05039188bef6a8b464fc3e2
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-08 07:09:57 +0530

    Time: 29 ms (25.75%) | Memory: 46 MB (32.93%) - LeetSync

--- 826-soup-servings/soup-servings.java
+++ 826-soup-servings/soup-servings.java
@@ -1,23 +1,34 @@
-public class Solution {
-    double[][] dp;
-
-    double dfs(int a, int b) {
-        if (a <= 0 && b > 0) return 1.0;
-        if (a <= 0 && b <= 0) return 0.5;
-        if (a > 0 && b <= 0) return 0.0;
-
-        if (dp[a][b] > 0) return dp[a][b];
-
-        return dp[a][b] = 0.25 * dfs(Math.max(a - 4, 0), b) +
-                          0.25 * dfs(Math.max(a - 3, 0), Math.max(b - 1, 0)) +
-                          0.25 * dfs(Math.max(a - 2, 0), Math.max(b - 2, 0)) +
-                          0.25 * dfs(Math.max(a - 1, 0), Math.max(b - 3, 0));
+class Solution {
+    Map<Long,Double> dp;
+    double dfs(int a,int b){
+        long key=(long)a<<32|b;
+        if(dp.containsKey(key)){
+            return dp.get(key);
+        }
+        if(a>0 && b==0){
+            dp.put(key,0.0);
+            return 0.0;
+        }
+        if(a==0 && b>0){
+            dp.put(key,1.0);
+            return 1.0;
+        }
+        if(a==0 && b==0){
+            dp.put(key,0.5);
+            return 0.5;
+        }
+        double val=0.25*dfs(Math.max(a-4,0),b)+0.25*dfs(Math.max(a-3,0),Math.max(b-1,0))+0.25*dfs(Math.max(a-2,0),Math.max(b-2,0))+0.25*dfs(Math.max(a-1,0),Math.max(b-3,0));
+        dp.put(key,val);
+        return val;
     }
-
     public double soupServings(int n) {
-        if (n >= 5000) return 1.0;
-        int units = (n + 24) / 25;
-        dp = new double[units + 1][units + 1];
-        return dfs(units, units);
+        if(n>=5000){
+            return 1.0;
+        }
+        int scaled=(n+24)/25;
+        //System.out.println(scaled);
+        dp=new HashMap<>();
+        return dfs(scaled,scaled);
+
     }
 }
