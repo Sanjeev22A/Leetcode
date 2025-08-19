7bb3feb15ed5b7d315d0a2d99c915c23f28b0c95
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-19 05:57:20 +0530

    Time: 3 ms (94.03%) | Memory: 61.2 MB (94.72%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -1,17 +1,14 @@
 class Solution {
-    long numSubArrays(int a){
-        long b=(long)a*(long)(a+1);
-        return b/2;
-    }
+   
     public long zeroFilledSubarray(int[] nums) {
-        //List<Integer> arr=new ArrayList<>();
+        
         int prev=-1,count=0;
         long ans=0;
         for(int a:nums){
             if(prev==a){
                 count++;
                 if(a==0){
-                    //System.out.println(count);
+                    
                     ans+=count;
                 }
             }else{
@@ -19,7 +16,7 @@ class Solution {
 
                 prev=a;
                 if(a==0){
-                    //System.out.println(1);
+                 
                     ans++;
                 }
             }
