53c76191410e6c8e87a012e473d5d6299c3eedfd
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-21 06:07:13 +0530

    Time: 11 ms (7.84%) | Memory: 54.9 MB (72.70%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,46 +1,25 @@
 class Solution {
-    int[][] addMatrix;
-    void makeAddMatrix(int[][] matrix){
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                addMatrix[i][j]=matrix[i][j];
-                if(i-1>=0){
-                    addMatrix[i][j]+=addMatrix[i-1][j];
-                }
-                if(j-1>=0){
-                    addMatrix[i][j]+=addMatrix[i][j-1];
-                }
-                if(i>0 && j>0){
-                    addMatrix[i][j]-=addMatrix[i-1][j-1];
-                }
-                System.out.print(addMatrix[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
-    boolean isTrue(int[][] matrix,int i,int j,int size){
-        int getSum=addMatrix[i+size-1][j+size-1];
-        if(i>0 && j>0) getSum+=addMatrix[i-1][j-1];
-        if(i>0) getSum-=addMatrix[i-1][j+size-1];
-        if(j>0) getSum-=addMatrix[i+size-1][j-1];
-        return getSum==size*size?true:false;
-    }
-    int logic(int[][] matrix){
-        int count=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                for(int size=1;i+size<=matrix.length && j+size<=matrix[0].length;size++){
-                    if(isTrue(matrix,i,j,size)){
-                        count++;
-                    }
-                }
-            }
-        }
-        return count;
-    }
+
     public int countSquares(int[][] matrix) {
-        addMatrix=new int[matrix.length][matrix[0].length];
-        makeAddMatrix(matrix);
-        return logic(matrix);
+        int m=matrix.length,n=matrix[0].length;
+        int ans=0;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int left=j>0?dp[i][j-1]:0;
+                int bottom=i>0?dp[i-1][j]:0;
+                int diagonal=i>0 && j>0?dp[i-1][j-1]:0;
+                int cur=Math.min(left,Math.min(bottom,diagonal));
+                if(matrix[i][j]==0){
+                    dp[i][j]=0;
+                }else{
+                    dp[i][j]=cur+1;
+                }
+                //System.out.println(dp[i][j]);
+                ans+=dp[i][j];
+            }
+        }
+        return ans;
+        
     }
 }
