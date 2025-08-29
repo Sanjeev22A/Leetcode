725fe853d7b9eb0386d692348681b7b3f173140e
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-08-29 05:59:27 +0530

    Time: 6 ms (43.81%) | Memory: 45.6 MB (57.52%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,30 +1,26 @@
 class Solution {
-
     public int[][] sortMatrix(int[][] grid) {
-        int n = grid.length;
-
-        for (int i = 0; i < n; i++) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int j = 0; i + j < n; j++) {
-                tmp.add(grid[i + j][j]);
+        int n=grid.length;
+        for(int i=0;i<n;i++){
+            List<Integer> temp=new ArrayList<>();
+            for(int j=0;i+j<n;j++){
+                temp.add(grid[i+j][j]);
             }
-            tmp.sort(Collections.reverseOrder());
-            for (int j = 0; i + j < n; j++) {
-                grid[i + j][j] = tmp.get(j);
-            }
-        }
-
-        for (int j = 1; j < n; j++) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int i = 0; j + i < n; i++) {
-                tmp.add(grid[i][j + i]);
-            }
-            Collections.sort(tmp);
-            for (int i = 0; j + i < n; i++) {
-                grid[i][j + i] = tmp.get(i);
+            Collections.sort(temp,(a,b)->(b-a));
+            for(int j=0;i+j<n;j++){
+                grid[i+j][j]=temp.get(j);
             }
         }
-
+        for(int j=1;j<n;j++){
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i+j<n;i++){
+                temp.add(grid[i][i+j]);
+            }
+            Collections.sort(temp);
+            for(int i=0;i+j<n;i++){
+                grid[i][i+j]=temp.get(i);
+            }
+        }
         return grid;
     }
 }
