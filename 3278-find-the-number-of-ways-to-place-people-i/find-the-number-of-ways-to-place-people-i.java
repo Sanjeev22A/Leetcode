3defbdcde71ad98625336aa9c223114e2cce6853
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-09-02 07:00:01 +0530

    Time: 13 ms (62.69%) | Memory: 44.9 MB (14.93%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -1,45 +1,29 @@
-public class Solution {
-
+class Solution {
     public int numberOfPairs(int[][] points) {
-        int ans = 0;
-        int n = points.length;
-
-        for (int i = 0; i < n; i++) {
-            int[] pointA = points[i];
-            for (int j = 0; j < n; j++) {
-                int[] pointB = points[j];
-                if (
-                    i == j ||
-                    !(pointA[0] <= pointB[0] && pointA[1] >= pointB[1])
-                ) {
+        int count=0;
+        for(int i=0;i<points.length;i++){
+            int[] a=points[i];
+            for(int j=0;j<points.length;j++){
+                int[] b=points[j];
+                if(i==j || !(b[0]<=a[0] && b[1]>=a[1])){
                     continue;
                 }
-                if (n == 2) {
-                    ans++;
-                    continue;
-                }
-
-                boolean illegal = false;
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) {
+                boolean flag=false;
+                for(int k=0;k<points.length;k++){
+                    int[] c=points[k];
+                    if(i==k || j==k){
                         continue;
                     }
-
-                    int[] pointTmp = points[k];
-                    boolean isXContained =
-                        pointTmp[0] >= pointA[0] && pointTmp[0] <= pointB[0];
-                    boolean isYContained =
-                        pointTmp[1] <= pointA[1] && pointTmp[1] >= pointB[1];
-                    if (isXContained && isYContained) {
-                        illegal = true;
+                    if(c[0]<=a[0] && c[1]>=a[1] && b[0]<=c[0] && b[1]>=c[1]){
+                        flag=true;
                         break;
                     }
                 }
-                if (!illegal) {
-                    ans++;
+                if(!flag){
+                    count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 }
