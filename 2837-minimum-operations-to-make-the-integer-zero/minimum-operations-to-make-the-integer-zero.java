45cc9abe84b0a70795b30efdbc0ee7b21ca79a80
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-09-05 08:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (100.00%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -1,10 +1,10 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
         for(int i=1;i<=60;i++){
-            long cur=(long)num1-(long)num2*i;
-            if(cur<i){
+            long cur=(long)num1-(long)num2*i; //i operations done
+            //If number is less than i we cant represent it in terms of power of i, if greater means we can liberally use 0s as much as needed and pad
+            if(cur<(long)i){
                 return -1;
-
             }
             if(i>=Long.bitCount(cur)){
                 return i;
