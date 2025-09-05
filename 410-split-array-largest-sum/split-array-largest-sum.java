d841e26d58cb6bf822b2a3af878be9bcc8309c3e
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-09-05 09:17:58 +0530

    Time: 4 ms (4.91%) | Memory: 41.3 MB (68.12%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    boolean isPossible(int[] nums,long allowed,int k){
+        int curSplitCount=1;
+        long runningTotal=0L;
+        for(int a:nums){
+            if(a>allowed){
+                return false;
+            }
+            else if(curSplitCount>k){
+                return false;
+            }
+            else if(runningTotal+(long)a>allowed){
+                runningTotal=a;
+                curSplitCount++;
+            }else{
+                runningTotal+=a;
+            }
+        }
+        return curSplitCount<=k;
+    }
+    public int splitArray(int[] nums, int k) {
+        long end=0L;
+        for(int a:nums){
+            end+=(long)a;
+        }
+        long start=0;
+        int ans=0;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            System.out.println(mid);
+            if(isPossible(nums,mid,k)){
+                ans=(int)mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+}
