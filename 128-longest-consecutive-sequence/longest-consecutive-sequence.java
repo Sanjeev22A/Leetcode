35149b4584c88bac27973097020c3e588f0531d2
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-09-13 15:12:30 +0530

    Time: 58 ms (8.11%) | Memory: 70.8 MB (5.83%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,59 +1,38 @@
-class Pair{
-    int val,count;
-    Pair(int val,int count){
-        this.val=val;
-        this.count=count;
-    }
-}
-
-class UnionFind{
-    Map<Integer,Pair> parent;
-    UnionFind(int[] nums){
-        parent=new HashMap<>();
-        for(int a:nums){
-            parent.put(a,new Pair(a,1));
-        }
-    }
-    int find(int a){
-        if(a==parent.get(a).val){
-            return a;
-        }
-        parent.get(a).val=find(parent.get(a).val);
-        return parent.get(a).val;
-    }
-
-    int union(int a,int b){
-        Pair rootA=parent.get(find(a));
-        Pair rootB=parent.get(find(b));
-        if(rootA.val==rootB.val){
-            return rootA.count;
-        }
-        parent.get(rootB.val).val=rootA.val;
-        parent.get(rootA.val).count=rootA.count+rootB.count;
-        return rootA.count+rootB.count;
-    }
-
-    int getMax(){
-        int max=0;
-        for(Map.Entry<Integer,Pair> a:parent.entrySet()){
-            max=Math.max(a.getValue().count,max);
-        }
-        return max;
-    }
-}
 class Solution {
     public int longestConsecutive(int[] nums) {
-        UnionFind uf=new UnionFind(nums);
-        HashSet<Integer> set=new HashSet<>();
+        Set<Integer> numSet=new HashSet<>();
+        Map<Integer,Integer> numMap=new HashMap<>();
         for(int a:nums){
-            set.add(a);
+            numSet.add(a);
         }
-        for(int a:set){
-            if(set.contains(a+1)){
-                uf.union(a,a+1);
-               
+        int max=0;
+        for(int a:numSet){
+            if(numMap.containsKey(a)){
+                continue;
             }
+            
+            int count=1;
+            int b=a;
+            while(numSet.contains(b-1)){
+                if(numMap.containsKey(b-1)){
+                    count=count+numMap.get(b-1);
+                    break;
+                }
+                count++;
+                b--;
+            }
+            max=Math.max(max,count);
+            b=a;
+            for(int i=count;i>0;i--){
+                if(numMap.containsKey(b)){
+                    break;
+                }
+                numMap.put(b,i);
+                b--;
+            }
+
         }
-        return uf.getMax();
+        return max;
+
     }
 }
