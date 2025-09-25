d7cc697f406af51a30e210af008e94bf3239a532
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-09-25 06:20:09 +0530

    Time: 211 ms (5.06%) | Memory: 55.6 MB (26.51%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -1,31 +1,45 @@
 class Solution {
     public String robotWithString(String s) {
         int[] map=new int[26];
-        char smallest='z'+1;
-        for(char c:s.toCharArray()){
+        char[] smallest=new char[s.length()];
+        char temp='z'+1;
+        for(int i=s.length()-1;i>=0;i--){
+            char c=s.charAt(i);
             map[c-'a']++;
-            if(c<smallest){
-                smallest=c;
-            }
+            temp=(char)Math.min(temp,c);
+            smallest[i]=temp;
         }
-        Stack<Character> t=new Stack<>();
-        StringBuilder res=new StringBuilder();
-        for(char c:s.toCharArray()){
-            map[c-'a']--;
-          
-            t.push(c);
-            while(smallest!='z' && map[smallest-'a']==0){
-                smallest++;
-            }
+        Stack<Character> B=new Stack<>();
+        StringBuilder C=new StringBuilder();
+        char min='z';
+        for(int i=0;i<s.length();i++){
+            char c=smallest[i];
+            min=(char)Math.min(c,min);
             
-            while(!t.isEmpty () && t.peek()<=smallest){
-                res.append(t.pop());
+            while(map[min-'a']==0 && min!='z'){
+                min++;
             }
+            if(!B.isEmpty() && B.peek()>s.charAt(i)){
+                B.push(s.charAt(i));
+                map[s.charAt(i)-'a']--;
+                while(!B.isEmpty() && B.peek()<=min){
+                    C.append(B.pop());
+                }
+            }else{
+                while(!B.isEmpty() && B.peek()<=min){
+                    C.append(B.pop());
+                }
+                B.push(s.charAt(i));
+                map[s.charAt(i)-'a']--;
+            }
+
+            
+            
+            
         }
-        
-        while(!t.isEmpty()){
-            res.append(t.pop());
+        while(!B.isEmpty()){
+            C.append(B.pop());
         }
-        return res.toString();
+        return C.toString();
     }
 }
