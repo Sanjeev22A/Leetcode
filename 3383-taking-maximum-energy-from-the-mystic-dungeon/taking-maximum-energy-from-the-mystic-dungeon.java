1c66b783c35ef4a2b58c2da3dfaf2dc19fa063bf
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-10-10 05:46:18 +0530

    Time: 2 ms (97.93%) | Memory: 62.6 MB (41.38%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,10 +1,6 @@
 class Solution {
-    int[] energy;
-    int k;
-    
+   
     public int maximumEnergy(int[] energy, int k) {
-        this.energy=energy;
-        this.k=k;
         int[] cache=new int[energy.length];
         int max=Integer.MIN_VALUE;
         for(int i=energy.length-1;i>=0;i--){
