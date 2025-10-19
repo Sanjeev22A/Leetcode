b14998b274e493246c9575d83a9b30758cfc7740
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-10-19 11:30:20 +0530

    Time: 333 ms (5.62%) | Memory: 60.1 MB (5.62%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    String doOperationA(String enter,int a){
+        StringBuilder temp=new StringBuilder();
+        for(int i=0;i<enter.length();i++){
+            if(i%2==0){
+                temp.append(enter.charAt(i));
+            }else{
+                int b=enter.charAt(i)-'0';
+                b=b+a;
+                b=b%10;
+                temp.append(""+b);
+            }
+        }
+        return temp.toString();
+    }
+    String doOperationB(String enter, int b) {
+        int n = enter.length();
+        b = b % n;
+        return enter.substring(n - b) + enter.substring(0, n - b);
+    }
+
+    Set<String> cache;
+    String minimum;
+    void doOps(String s,int a,int b,boolean flag){
+        if(cache.contains(s+":"+flag)){
+            return;
+        }
+        String op1;
+        if(flag){
+            op1=doOperationA(s,a);
+        }else{
+            op1=doOperationB(s,b);
+        }
+        if(op1.compareTo(minimum)<0){
+            minimum=new String(op1);
+        }
+        //System.out.println(op1);
+        cache.add(s+":"+flag);
+        doOps(op1,a,b,false);
+        doOps(op1,a,b,true);
+        
+    }
+    public String findLexSmallestString(String s, int a, int b) {
+        minimum=new String(s);
+        cache=new HashSet<>();
+        doOps(s,a,b,true);
+        doOps(s,a,b,false);
+        return minimum;
+    }
+}
