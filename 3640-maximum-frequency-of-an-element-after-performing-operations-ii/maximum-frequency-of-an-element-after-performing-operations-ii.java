7091866c4cbfcda6fe3519f4e638a96bc1a6d748
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-10-23 07:15:14 +0530

    Time: 381 ms (56.31%) | Memory: 75.1 MB (53.40%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        List<int[]> arr=new ArrayList<>();
+        Map<Integer,Integer> freqMap=new HashMap<>();
+        for(int a:nums){
+            freqMap.put(a,freqMap.getOrDefault(a,0)+1);
+            arr.add(new int[]{a-k,1});
+            arr.add(new int[]{a,0});
+            arr.add(new int[]{a+k+1,-1});
+        }
+        Collections.sort(arr,(a,b)->{
+            if(a[0]!=b[0]){
+                return Integer.compare(a[0],b[0]);
+            }
+            return Integer.compare(a[1],b[1]);
+        });
+        int runningSum=0;
+        int max=0;
+        for(int[] a:arr){
+            runningSum+=a[1];
+            int opCount=runningSum-freqMap.getOrDefault(a[0],0);
+            //System.out.println(a[0]+":"+opCount);
+            if(opCount>numOperations){
+                max=Math.max(numOperations+freqMap.getOrDefault(a[0],0),max);
+            }else{
+                max=Math.max(runningSum,max);
+            }
+        }
+        return max;
+    }
+}
