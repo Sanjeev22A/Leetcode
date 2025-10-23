f7015613b5e74d3e6e37e726e031d6eda972e0df
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-10-23 07:15:58 +0530

    Time: 454 ms (13.74%) | Memory: 81 MB (11.13%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,34 +1,30 @@
 class Solution {
-    int Max=(int)Math.pow(10,5)+2;
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        Arrays.sort(nums);
-        int[] freqMap=new int[Max];
-        int[] sweepMap=new int[Max];
-        int[] numOps=new int[Max];
-        int ans=0;
+        List<int[]> arr=new ArrayList<>();
+        Map<Integer,Integer> freqMap=new HashMap<>();
         for(int a:nums){
-            freqMap[a]++;
-            int lower=Math.max(0,a-k);
-            int upper=Math.min(Max-1,a+k+1);
-            numOps[lower]+=1;
-            
-            sweepMap[lower]++;
-            sweepMap[upper]--;
-            numOps[upper]-=1;
+            freqMap.put(a,freqMap.getOrDefault(a,0)+1);
+            arr.add(new int[]{a-k,1});
+            arr.add(new int[]{a,0});
+            arr.add(new int[]{a+k+1,-1});
         }
-        for(int i=1;i<Max;i++){
-            sweepMap[i]+=sweepMap[i-1];
-            numOps[i]+=numOps[i-1];
-        }
-        for(int i=0;i<Max;i++){
-            //System.out.println(i+":"+sweepMap[i]+":"+numOps[i]);
-            
-            if(numOps[i]-freqMap[i]>numOperations){
-                ans=Math.max(sweepMap[i]-(numOps[i]-freqMap[i]-numOperations),ans);
+        Collections.sort(arr,(a,b)->{
+            if(a[0]!=b[0]){
+                return Integer.compare(a[0],b[0]);
+            }
+            return Integer.compare(a[1],b[1]);
+        });
+        int runningSum=0;
+        int max=0;
+        for(int[] a:arr){
+            runningSum+=a[1];
+            int opCount=runningSum-freqMap.getOrDefault(a[0],0);
+            if(opCount>numOperations){
+                max=Math.max(numOperations+freqMap.getOrDefault(a[0],0),max);
             }else{
-                ans=Math.max(sweepMap[i],ans);
-            }   
+                max=Math.max(runningSum,max);
+            }
         }
-        return ans;
+        return max;
     }
 }
