697a10ef60c40fb87a6e9e7746b2aad9a361d5dc
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-11-02 08:04:41 +0530

    Time: 20 ms (83.94%) | Memory: 147.2 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -5,37 +5,50 @@ class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         int[][] map=new int[m][n];
         int[][] dir=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
-        Queue<int[]> q=new LinkedList<>();
-        for(int[] a:guards){
-            map[a[0]][a[1]]=1;
-            q.offer(a);
-        }
+        
+        
         for(int[] a:walls){
             map[a[0]][a[1]]=-1;
         }
-        while(!q.isEmpty()){
-            int[] a=q.poll();
-            if(map[a[0]][a[1]]==-1){
-                continue;
+        for(int[] a:guards){
+            map[a[0]][a[1]]=1;
+        }
+        for(int[] a:guards){
+            
+            for(int i=a[0]+1;i<m;i++){
+                if(map[i][a[1]]!=-1 && map[i][a[1]]!=1){
+                    map[i][a[1]]=2;
+                }else{
+                    break;
+                }
             }
-            if(map[a[0]][a[1]]==1){
-                int c=2;
-                for(int[] b:dir){
-                    int nx=a[0]+b[0];
-                    int ny=a[1]+b[1];
-                    while(isMemorySafe(nx,ny,m,n) && map[nx][ny]!=-1 && map[nx][ny]!=1){
-                        map[nx][ny]=c;
-                        nx+=b[0];
-                        ny+=b[1];
-                    }
-                    
+            for(int i=a[0]-1;i>=0;i--){
+                if(map[i][a[1]]!=-1 && map[i][a[1]]!=1){
+                    map[i][a[1]]=2;
+                }else{
+                    break;
+                }
+            }
+            for(int j=a[1]+1;j<n;j++){
+                if(map[a[0]][j]!=-1 && map[a[0]][j]!=1){
+                    map[a[0]][j]=2;
+                }else{
+                    break;
+                }
+            }
+            for(int j=a[1]-1;j>=0;j--){
+                if(map[a[0]][j]!=-1 && map[a[0]][j]!=1){
+                    map[a[0]][j]=2;
+                }else{
+                    break;
                 }
             }
         }
+
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-    
+        
                 if(map[i][j]==0){
                     ans++;
                 }
