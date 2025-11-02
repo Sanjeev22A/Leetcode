d72a6b32da373ffa7fc195c8c2c0834b055f8c5b
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-11-02 07:55:02 +0530

    Time: 37 ms (20.44%) | Memory: 149 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,31 +1,47 @@
 class Solution {
+    boolean isMemorySafe(int i,int j,int m,int n){
+        return i>=0 && i<m && j>=0 && j<n;
+    }
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] grid = new int[m][n];
-        // 0 = empty, -1 = wall, 1 = guard, 2 = guarded
-        
-        for (int[] w : walls) grid[w[0]][w[1]] = -1;
-        for (int[] g : guards) grid[g[0]][g[1]] = 1;
-
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        
-        for (int[] g : guards) {
-            for (int[] d : dirs) {
-                int x = g[0] + d[0];
-                int y = g[1] + d[1];
-                while (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != -1 && grid[x][y] != 1) {
-                    if (grid[x][y] == 0) grid[x][y] = 2; // mark guarded
-                    x += d[0];
-                    y += d[1];
+        int[][] map=new int[m][n];
+        int[][] dir=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        Queue<int[]> q=new LinkedList<>();
+        for(int[] a:guards){
+            map[a[0]][a[1]]=1;
+            q.offer(a);
+        }
+        for(int[] a:walls){
+            map[a[0]][a[1]]=-1;
+        }
+        while(!q.isEmpty()){
+            int[] a=q.poll();
+            if(map[a[0]][a[1]]==-1){
+                continue;
+            }
+            if(map[a[0]][a[1]]==1){
+                int c=2;
+                for(int[] b:dir){
+                    int nx=a[0]+b[0];
+                    int ny=a[1]+b[1];
+                    while(isMemorySafe(nx,ny,m,n) && map[nx][ny]!=-1 && map[nx][ny]!=1){
+                        map[nx][ny]=c;
+                        nx+=b[0];
+                        ny+=b[1];
+                    }
+                    
                 }
             }
         }
-
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) ans++;
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+    
+                if(map[i][j]==0){
+                    ans++;
+                }
             }
         }
         return ans;
+        
     }
 }
