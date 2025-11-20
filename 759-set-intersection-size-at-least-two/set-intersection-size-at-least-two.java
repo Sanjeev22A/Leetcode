b2900af12ebfcdb45825553b5c159b394fa77d44
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-11-20 08:00:11 +0530

    Time: 79 ms (8.16%) | Memory: 53.3 MB (11.56%) - LeetSync

---
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int intersectionSizeTwo(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->{
+            if(a[1]!=b[1]){
+                return Integer.compare(a[1],b[1]);
+            }
+            return Integer.compare(a[1],b[1]);
+        });
+        int a=-1,b=-1;
+        int count=0;
+        for(int[] c:intervals){
+            System.out.println(a+":"+b+":"+c[0]+":"+c[1]);
+            if(a==-1 && b==-1){
+                a=c[1]-1;
+                b=c[1];
+         
+                count+=2;
+            }else{
+                if(a>=c[0] && a<=c[1]){
+                    continue;
+                }
+                if(a<c[0]){
+                    if(b==c[0]){
+                        a=c[0];
+                        b=c[1];
+                    
+                    }else{
+                        if(b==c[1]-1){
+                            continue;
+                        }
+                        a=c[1]-1;
+                       
+                      
+                    }
+                    
+                    count++;
+                }
+                if(b<c[0]){
+                    b=c[1];
+                  
+                    count++;
+                }
+            }
+            int d=a;
+            a=Math.min(a,b);
+            b=Math.max(d,b);
+        }
+        return count;
+    }
+}
