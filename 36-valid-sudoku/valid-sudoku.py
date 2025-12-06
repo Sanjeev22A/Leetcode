42ccee29b9f8435b05b8ae198dc619100f003ef6
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2025-12-06 16:52:29 +0530

    Time: 4 ms (56.28%) | Memory: 18 MB (5.33%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,15 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [[False] * 9 for _ in range(9)]
-        cols = [[False] * 9 for _ in range(9)]
-        boxes = [[False] * 9 for _ in range(9)]
+        
+        if(len(board)!=9 or any(len(row)!=9 for row in board)):
+            return False
+
+        rows=[[False]*9 for _ in range(9)]
+        cols=[[False]*9 for _ in range(9)]
+        boxes=[[False]*9 for _ in range(9)]
 
         for i in range(9):
             for j in range(9):
-                if board[i][j] != '.':
-                    num = ord(board[i][j]) - ord('1')
-                    boxIndex = (i // 3) * 3 + (j // 3)
-                    if rows[i][num] or cols[j][num] or boxes[boxIndex][num]:
+                if board[i][j]!='.':
+                    num=ord(board[i][j])-ord('1')
+                    box_index=(i//3)*3+(j//3)
+                    if rows[i][num] or cols[j][num] or boxes[box_index][num]:
                         return False
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = True
+                    rows[i][num]=cols[j][num]=boxes[box_index][num]=True
         return True
