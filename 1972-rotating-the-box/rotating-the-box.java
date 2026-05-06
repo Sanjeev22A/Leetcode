d4ae6b69c134675cc3c7a1c007f981cc3b21e05e
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2026-05-06 07:05:48 +0530

    Time: 545 ms (5.05%) | Memory: 147 MB (5.53%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,29 @@
+class Solution {
+    void rotate(int m,int j,char[] row,char[][] newGrid){
+        int nextFree=row.length-1;
+        for(int i=row.length-1;i>=0;i--){
+            if(row[i]=='*'){
+                nextFree=i-1;
+            }else if(row[i]=='#'){
+                System.out.println(nextFree);
+                if(i!=nextFree){
+                    row[nextFree]=row[i];
+                    row[i]='.';
+                }
+                nextFree--;
+            }
+        }
+        for(int i=0;i<row.length;i++){
+            newGrid[i][m-j-1]=row[i];
+        }
+    }
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m=boxGrid.length;
+        int n=boxGrid[0].length;
+        char[][] ans=new char[n][m];
+        for(int i=0;i<m;i++){
+            rotate(m,i,boxGrid[i],ans);
+        }
+        return ans;
+    }
+}
