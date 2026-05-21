fc9a9a08aff7432095b1545c31f48e22feba10a9
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2026-05-21 06:54:51 +0530

    Time: 371 ms (6.91%) | Memory: 81.7 MB (52.75%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,23 +1,47 @@
 class Solution {
+    List<Integer> getAllPrefix(int a){
+        List<Integer> ans=new ArrayList<>();
+        String s=Integer.toString(a);
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            sb.append(s.charAt(i));
+            ans.add(Integer.parseInt(sb.toString()));
+        }
+        return ans;
+    }
+    void addPrefix(int a,Set<Integer> s1){
+        List<Integer> prefix=getAllPrefix(a);
+        for(int b:prefix){
+            s1.add(b);
+        }
+    }
+    int length(int b){
+        int i=0;
+        while(b>0){
+            b/=10;
+            i++;
+        }
+        return i;
+    }
+    int getMaxLength(int a,Set<Integer> s1){
+        int max=0;
+        List<Integer> prefix=getAllPrefix(a);
+        for(int b:prefix){
+            if(s1.contains(b)){
+                max=Math.max(max,length(b));
+            }
+        }
+        return max;
+    }
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<String> prefixes=new HashSet<>();
-        for(int e:arr1){
-            String prefix="";
-            for(char c:String.valueOf(e).toCharArray()){
-                prefix+=c;
-                prefixes.add(prefix);
-            }
+        Set<Integer> s1=new HashSet<>();
+        for(int a:arr1){
+            addPrefix(a,s1);
         }
-        int maxLength=0;
-        for(int e:arr2){
-            String prefix="";
-            for(char c:String.valueOf(e).toCharArray()){
-                prefix+=c;
-                if(prefixes.contains(prefix)){
-                    maxLength=Math.max(maxLength,prefix.length());
-                }
-            }
+        int max=0;
+        for(int a:arr2){
+            max=Math.max(max,getMaxLength(a,s1));
         }
-        return maxLength;
+        return max;
     }
 }
