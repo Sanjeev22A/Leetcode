f04f6cb665963b1a97cdf8b49ccb81148976fb66
SanjeevA <152994973+Sanjeev22A@users.noreply.github.com>
2026-06-23 07:04:58 +0530

    Time: 242 ms (5.10%) | Memory: 101.8 MB (51.02%) - LeetSync

---
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.java
@@ -0,0 +1,45 @@
+class Solution {
+    int getParent(int a){
+        return a/2;
+    }
+    //Returns the depth if b is child of a, else -1
+    int isChild(int a,int b){
+        int c=getParent(b);
+        if(b==a){
+            return 1;
+        }else if(b<a){
+            return -1;
+        }else{
+            int d=isChild(a,c);
+            if(d==-1){
+                return -1;
+            }
+            return 1+d;
+        }
+    }
+
+    int getCycleLength(int a,int b){
+        //System.out.println(a+":"+b+":"+isChild(a,b));
+        if(a>b){
+            return getCycleLength(b,a);
+        }else if(a==b){
+            return 0;
+        }
+        int c=isChild(a,b);
+        if(c!=-1){
+            return c;
+        }else{
+            return 1+getCycleLength(getParent(a),b);
+        }
+    }
+    public int[] cycleLengthQueries(int n, int[][] queries) {
+
+        int[] ans=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int[] a=queries[i];
+            ans[i]=getCycleLength(a[0],a[1]);
+           // System.out.println("==========");
+        }
+        return ans;
+    }
+}
